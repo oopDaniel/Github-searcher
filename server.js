6dde73470d9f833b1f846acc62d43d1672f1c6e6
Daniel <oopdaniel@gmail.com>
2017-01-24 04:12:08 +0800

    production ready

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ import morgan from 'morgan';
 import helmet  from 'helmet';
 
 const app = express();
+const distPath = path.join(__dirname, 'dist');
 
+const rewritePath = (req, res, next) => {
+  req.url = req.url.replace(/\/detail\/static\//, '/');
+  next();
+};
+
+
+// Webpack related
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
   const webpack = require('webpack');
@@ -19,15 +27,25 @@ if (process.env.NODE_ENV !== 'production') {
 } else {
   app.use(morgan('combined'));
   app.use(helmet());
-  app.use('/static', express.static(path.join(__dirname, 'dist')));
+  app.use('/static', express.static(distPath));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+
+// Router
+app.get('*', (req, res, next) => {
+  const ext = path.extname(req.url);
+  const shouldServeHTML = ext !== '.js' && ext !== '.css';
+  if (shouldServeHTML) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  } else {
+    next();
+  }
+}, rewritePath, express.static(distPath));
+
 
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
+
 
 app.listen(port, host, (err) => {
   if (err) {
