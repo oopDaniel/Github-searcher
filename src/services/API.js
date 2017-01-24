de83f102c2ae3b51e0c369d5690a07d33d020c32
Daniel <oopdaniel@gmail.com>
2017-01-24 16:36:29 +0800

    make fetch API throwable

--- src/services/API.js
+++ src/services/API.js
@@ -1,26 +1,37 @@
 /* eslint-disable space-in-parens */
-import fetch from 'isomorphic-fetch';
+
+/* ***************************
+ *
+ *    Suppress the error handling
+ *    so that saga can catch correctly
+ *
+ *
+**/
+
+import fetch from './fetch';
 
 const API = {
   getUser(target) {
     const url = `https://api.github.com/search/users?q=${target}`;
     return fetch(url)
-      .then( response => response.json() )
-      .catch( error => ({ error }) );
+      .then( response => response.json() );
   },
 
   getUserDetail(target) {
     const url = `https://api.github.com/users/${target}`;
     return fetch(url)
-      .then( response => response.json() )
-      .catch( error => ({ error }) );
+      .then( response => response.json() );
   },
 
   getRepos(target) {
     const url = `https://api.github.com/users/${target}/repos`;
     return fetch(url)
-      .then( response => response.json() )
-      .catch( error => ({ error }) );
+      .then( response => response.json() );
+  },
+  getReadme(user, repo) {
+    const url = `https://api.github.com/repos/${user}/${repo}/readme`;
+    return fetch(url)
+      .then( response => response.json() );
   },
 };
 
